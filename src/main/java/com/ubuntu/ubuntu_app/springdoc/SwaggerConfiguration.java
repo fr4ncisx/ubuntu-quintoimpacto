c2f5c79008264ef6aab8a7f1f8ea72894fb291df
fr4ncisx <fr4ncisx@gmail.com>
2024-08-02 05:23:28 -0300

    Cambios Swagger UI

--- src/main/java/com/ubuntu/ubuntu_app/springdoc/SwaggerConfiguration.java
+++ src/main/java/com/ubuntu/ubuntu_app/springdoc/SwaggerConfiguration.java
@@ -1,5 +1,7 @@
 package com.ubuntu.ubuntu_app.springdoc;
 
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springdoc.webmvc.api.OpenApiResource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,12 +16,19 @@ public class SwaggerConfiguration {
         @Bean
         public OpenAPI customOpenAPI() {
                 return new OpenAPI()
-                        .components(new Components()
-                             .addSecuritySchemes("bearer-key",
-                             new SecurityScheme()
-                                  .type(SecurityScheme.Type.HTTP)
-                                  .scheme("bearer")
-                                  .bearerFormat("JWT")))
-                        .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
+                                .components(new Components()
+                                                .addSecuritySchemes("bearer-key",
+                                                                new SecurityScheme()
+                                                                                .type(SecurityScheme.Type.HTTP)
+                                                                                .scheme("bearer")
+                                                                                .bearerFormat("JWT")))
+                                .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
+        }
+        @Bean
+        public GroupedOpenApi publicApi() {
+            return GroupedOpenApi.builder()
+                    .group("public")
+                    .pathsToMatch("/**")
+                    .build();
         }
 }
