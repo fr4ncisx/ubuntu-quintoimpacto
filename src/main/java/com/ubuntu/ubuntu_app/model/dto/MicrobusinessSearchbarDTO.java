0948ed50817a10f5ee01bb97c4d1e0f014c908c0
fr4ncisx <fr4ncisx@gmail.com>
2024-08-12 23:20:34 -0300

    Fix DTO order

--- src/main/java/com/ubuntu/ubuntu_app/model/dto/MicrobusinessSearchbarDTO.java
+++ src/main/java/com/ubuntu/ubuntu_app/model/dto/MicrobusinessSearchbarDTO.java
@@ -3,7 +3,6 @@ package com.ubuntu.ubuntu_app.model.dto;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.time.LocalDate;
 
 import lombok.AllArgsConstructor;
@@ -14,16 +13,26 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MicrobusinessSearchbarDTO {
+    @JsonProperty(value = "id")
     private Long id;
+    @JsonProperty(value = "nombre")
     private String nombre;
+    @JsonProperty(value = "descripcion")
     private String descripcion;
+    @JsonProperty(value = "masInformacion")
     private String masInformacion;
+    @JsonProperty(value = "pais")
     private String pais;
+    @JsonProperty(value = "provincia")
     private String provincia;
+    @JsonProperty(value = "ciudad")
     private String ciudad;
+    @JsonProperty(value = "categoria")
     private CategoryDTO categoria;
+    @JsonProperty(value = "subcategoria")
     private String subcategoria;
-    @JsonProperty("fecha_creacion")
+    @JsonProperty(value = "fecha_creacion")
     private LocalDate fecha;
+    @JsonProperty(value = "imagenes")
     private List<ImageDTO> imagenes;
 }
