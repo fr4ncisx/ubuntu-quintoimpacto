0d95ebb919167c2f8cb5cd9307e9f201b47d377a
fr4ncisx <fr4ncisx@gmail.com>
2024-08-12 22:52:11 -0300

    Fixed DTO showing date

--- src/main/java/com/ubuntu/ubuntu_app/model/dto/MicrobusinessSearchbarDTO.java
+++ src/main/java/com/ubuntu/ubuntu_app/model/dto/MicrobusinessSearchbarDTO.java
@@ -1,6 +1,7 @@
 package com.ubuntu.ubuntu_app.model.dto;
 
 import java.util.List;
+import java.time.LocalDate;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,5 +20,6 @@ public class MicrobusinessSearchbarDTO {
     private String ciudad;
     private CategoryDTO categoria;
     private String subcategoria;
+    private LocalDate fecha_creacion;
     private List<ImageDTO> imagenes;
 }
