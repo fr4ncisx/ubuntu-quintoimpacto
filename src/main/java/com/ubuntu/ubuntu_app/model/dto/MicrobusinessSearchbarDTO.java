73726cd975c34cf92bdeba1022b90264375bd681
fr4ncisx <fr4ncisx@gmail.com>
2024-08-12 23:28:21 -0300

    Fix using @JsonPropertyOrder, to avoid repeating @JsonProperty at each attribute

--- src/main/java/com/ubuntu/ubuntu_app/model/dto/MicrobusinessSearchbarDTO.java
+++ src/main/java/com/ubuntu/ubuntu_app/model/dto/MicrobusinessSearchbarDTO.java
@@ -3,6 +3,8 @@ package com.ubuntu.ubuntu_app.model.dto;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.time.LocalDate;
 
 import lombok.AllArgsConstructor;
@@ -12,27 +14,19 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonPropertyOrder({ "id", "nombre", "descripcion", "masInformacion", "pais", "provincia", "ciudad", "categoria",
+        "subcategoria", "fecha_creacion", "imagenes" })
 public class MicrobusinessSearchbarDTO {
-    @JsonProperty(value = "id")
     private Long id;
-    @JsonProperty(value = "nombre")
     private String nombre;
-    @JsonProperty(value = "descripcion")
     private String descripcion;
-    @JsonProperty(value = "masInformacion")
     private String masInformacion;
-    @JsonProperty(value = "pais")
     private String pais;
-    @JsonProperty(value = "provincia")
     private String provincia;
-    @JsonProperty(value = "ciudad")
     private String ciudad;
-    @JsonProperty(value = "categoria")
     private CategoryDTO categoria;
-    @JsonProperty(value = "subcategoria")
     private String subcategoria;
     @JsonProperty(value = "fecha_creacion")
     private LocalDate fecha;
-    @JsonProperty(value = "imagenes")
     private List<ImageDTO> imagenes;
 }
