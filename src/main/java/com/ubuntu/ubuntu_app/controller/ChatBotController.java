28452fbf96d8f0fb815080c2210434e864f06883
fr4ncisx <fr4ncisx@gmail.com>
2024-08-22 14:08:17 -0300

    Update missing chatbot controllers

--- src/main/java/com/ubuntu/ubuntu_app/controller/ChatBotController.java
+++ src/main/java/com/ubuntu/ubuntu_app/controller/ChatBotController.java
@@ -1,30 +1,33 @@
 package com.ubuntu.ubuntu_app.controller;
 
-import com.ubuntu.ubuntu_app.infra.statuses.BotChatDoc;
 import com.ubuntu.ubuntu_app.service.ChatBotService;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Chatbot")
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/chatbot")
 public class ChatBotController {
 
-    @Autowired
-    private ChatBotService botService;
+    private final ChatBotService chatbotService;
 
-    @Operation(summary = "Hacer una pregunta al bot")
-    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(defaultValue = BotChatDoc.bot_chatter)))
-    @GetMapping("/faq")
-    public ResponseEntity<?> chatbotResponse(@RequestParam String userQuestion) {
-        return botService.getResponse(userQuestion);
+    @GetMapping("/init")
+    public ResponseEntity<?> chatbotResponse(@RequestParam String question) {
+        return chatbotService.getResponse(question);
     }
 
+    @GetMapping("/category")
+    public ResponseEntity<?> chatbotCategory(@RequestParam String name) {
+        return chatbotService.obtainQuestions(name);
+    }
+
+    @GetMapping("/response")
+    public ResponseEntity<?> responseDefaultQuestions(@RequestParam Long id) {
+        return chatbotService.answerResponseByIdAndFilteredCategory(id);
+    }
 }
