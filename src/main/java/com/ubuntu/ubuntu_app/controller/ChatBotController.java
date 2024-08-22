2a3f585d9572b43da30d93e6b7051a2a74da9576
fr4ncisx <fr4ncisx@gmail.com>
2024-08-22 14:58:30 -0300

    Swagger Update

--- src/main/java/com/ubuntu/ubuntu_app/controller/ChatBotController.java
+++ src/main/java/com/ubuntu/ubuntu_app/controller/ChatBotController.java
@@ -2,6 +2,7 @@ package com.ubuntu.ubuntu_app.controller;
 
 import com.ubuntu.ubuntu_app.service.ChatBotService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -16,16 +17,19 @@ public class ChatBotController {
 
     private final ChatBotService chatbotService;
 
+    @Operation(summary = "Interacturar con el chatbot libremente", description = "Se esperan preguntas libres para responder por el chatbot")
     @GetMapping("/init")
     public ResponseEntity<?> chatbotResponse(@RequestParam String question) {
         return chatbotService.getResponse(question);
     }
 
+    @Operation(summary = "Buscar preguntas por categoría")
     @GetMapping("/category")
     public ResponseEntity<?> chatbotCategory(@RequestParam String name) {
         return chatbotService.obtainQuestions(name);
     }
 
+    @Operation(summary = "Obtener respuesta por id")
     @GetMapping("/response")
     public ResponseEntity<?> responseDefaultQuestions(@RequestParam Long id) {
         return chatbotService.answerResponseByIdAndFilteredCategory(id);
