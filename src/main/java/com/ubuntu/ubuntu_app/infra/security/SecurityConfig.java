4c61cddac5a9b7309eb0cef765230dfa50b15750
fr4ncisx <fr4ncisx@gmail.com>
2024-08-23 15:41:45 -0300

    Fixed SpringBoot 3.3.3 problems with CORS not allowing authenticated endpoints

--- src/main/java/com/ubuntu/ubuntu_app/infra/security/SecurityConfig.java
+++ src/main/java/com/ubuntu/ubuntu_app/infra/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,6 +23,7 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
         return security
                 .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authRequest -> {
                     // TODO: Usuarios
